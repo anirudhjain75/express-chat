5fb18d9fb3fc9b131d87b515bf074fd354032b51
Anirudh Jain <anirudh.jain@outlook.com>
2018-09-09 15:33:01 +0530

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-    
-    socket.emit('createMessage', {
-        to: 'anirudh@ani.com',
-        text: 'Hey I am the browser'
-    });
-
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,15 +14,14 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('new user connected');
-
-  socket.emit('newMessage', {
-    from: 'anirudhjain@ani.com',
-    text: 'Hey there',
-    createdAt: 12345
-  });
   
   socket.on('createMessage', function (message) {
     console.log('Message has been created', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   });
 
   socket.on('disconnect',function () {
