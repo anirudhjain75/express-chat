8cac634058d8d2f22e8e2cbcebac109a2cd554e2
Anirudh Jain <anirudh.jain@outlook.com>
2018-09-09 20:16:15 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('new user connected');
+
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined'
+  });
   
   socket.on('createMessage', function (message) {
     console.log('Message has been created', message);
@@ -22,6 +32,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     })
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect',function () {
