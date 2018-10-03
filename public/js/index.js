07c6380cfdf33f07bde8af0c87721b6c7b203a39
Anirudh Jain <anirudh.jain@outlook.com>
2018-10-03 20:36:38 +0530

    scroll if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom () {
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight>= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -17,7 +33,8 @@ socket.on('newMessage', function (message) {
         createdAt: formattedTime
     });
 
-    jQuery('#messages').append(html)
+    jQuery('#messages').append(html);
+    scrollToBottom();
 
 
     // var li = jQuery('<li></li>');
@@ -44,6 +61,7 @@ socket.on('newLocationMessage', function (message) {
     // li.append(a);
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
